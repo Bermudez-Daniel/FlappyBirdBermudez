a9a41b09eab392dcdcf7af2532eb158bc0cb7fd1
Daniel Bermudez <144481244+Bermudez-Daniel@users.noreply.github.com>
2023-12-12 11:48:50 -0800

    Added scripts for gamecontroller
    Added text messages and attached game controller to controller script.

---
+++ FlappyBirdBermudezP3/Assets/Scripts/Game Control script.cs
@@ -0,0 +1,44 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControlscript : MonoBehaviour {
+
+    public static GameControlscript instance;
+    public GameObject gameOverText;
+    public bool gameOver = false;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update() 
+    {
+        if (gameOver == true && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+    } 
+     
+     
+     public void BirdDied()
+        {
+            gameOverText.SetActive(true);
+        gameOver = true;
+        }
+    
+}
